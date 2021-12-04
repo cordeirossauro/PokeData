175d9d0c66abada41e3e01fb88e31f9c7bb3b6bc
Vinicius Cordeiro <marcos.cordeiroal@gmail.com>
2021-12-04 11:55:02 -0300

    Add a progress bar function

---
+++ functions/progress_bar.py
@@ -0,0 +1,26 @@
+import math
+import time
+
+class ProgressBar:
+    def __init__(self, total_pokemon, current_pokename, current_pokenumber, bar_size):
+        self.total_pokemon = int(total_pokemon)
+        self.current_pokename = current_pokename
+        self.current_pokenumber = current_pokenumber
+        self.bar_size = bar_size
+
+
+    def print_bar(self):
+        steps_done = math.floor(self.current_pokenumber/self.total_pokemon * self.bar_size)
+        
+        bar = " [" + ("=" * steps_done) + ">" + (" " * (self.bar_size - 1 - steps_done)) + "]"
+        progress = f" [{self.current_pokenumber}/{self.total_pokemon}]"
+        details = f" Scraping data for {self.current_pokename}"
+
+        print(progress + bar + details + 10 * " ", end = "\r")
+
+
+    def update_bar(self, current_pokename, current_pokenumber):
+
+        self.current_pokename = current_pokename
+        self.current_pokenumber = current_pokenumber
+        self.print_bar()
