b71288975869f5a03f99d0ae5c31ebd7cb779a8b
Vinicius Cordeiro <marcos.cordeiroal@gmail.com>
2021-12-04 11:56:34 -0300

    Add a progress bar to the screen output

--- create_pokedex.py
+++ create_pokedex.py
@@ -12,43 +12,34 @@ try:
     sys.path.append("functions")
     import scrapers as scrp
     import extractors as ext
+    import progress_bar as pb
 except ModuleNotFoundError:
     print("Could not find the necessary functions inside their folder...")
     time.sleep(1)
     sys.exit()
 
 
-
 def pokecrawler(pokelist, verbose = 1):
-
-    total_pokemon = str(len(pokelist))
     if verbose == 1:
+        total_pokemon = str(len(pokelist))
         print(f"Looks like there's {total_pokemon} pokemon to read in the website...")
         time.sleep(1)
         print("Our Spinaraks will start going through them now!\n")
         time.sleep(1)
+        progress_bar = pb.ProgressBar(total_pokemon = total_pokemon,
+                                      current_pokename = "-", 
+                                      current_pokenumber = 0, 
+                                      bar_size = 30)
 
-
-    pokedata = pd.DataFrame()
     index = 1
     for number in pokelist.index:
         if verbose == 1:
             name = pokelist.loc[number]["name"]
-            print(" " * 50, end = "\r")
-            print(f"[{index}/{total_pokemon}] Scraping data for {name}", end = '\r')
+            progress_bar.update_bar(current_pokename = name, current_pokenumber = index)
 
         pokesoup = scrp.scrape_pokesoup(pokelist.loc[number]["link"])
 
-        gen_inf = ext.gen_inf_extractor(pokesoup)
-        phys_inf = ext.phys_inf_extractor(pokesoup)
-        ment_inf = ext.ment_inf_extractor(pokesoup)
-        abilities = ext.abilities_extractor(pokesoup)
-        weaknesses = ext.weaknesses_extractor(pokesoup)
-        stats = ext.stats_extractor(pokesoup)
-
-        pokedata = pd.concat([gen_inf, phys_inf, ment_inf,
-                              abilities, weaknesses, stats],
-                             axis = 1)
+        pokedata = ext.full_extractor(pokesoup)
         pokedata["number"] = number
         pokedata.set_index("number", inplace = True)
 
@@ -69,6 +60,6 @@ if __name__ == "__main__":
     gen_1to7_list = scrp.create_gen_1to7_list()
     gen_8_list = scrp.create_gen_8_list()
     pokelist = pd.concat([gen_1to7_list, gen_8_list], axis = 0)
-
+    
     os.makedirs("data", exist_ok = True)
     pokecrawler(pokelist)
